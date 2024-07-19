e664f86ed70b483b905d29f27866761175e9f47c
elieldiniz <elieldiniz1@outlook.com>
2024-07-19 18:06:00 -0400

    add interface env

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env.config.interface';
+import { ConfigService } from '@nestjs/config';
+
+
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+
+  constructor(private configService: ConfigService) {
+
+  }
+  getappPort(): number {
+    return Number(this.configService.get<number>('PORT'));
+}
+
+  getNodeEnv(): string {
+    return String(this.configService.get<string>('NODE_ENV'));
+  }
+}
