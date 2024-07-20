0dc562ae9b1787d538a32f0036ad7e442a8c8f63
elieldiniz <elieldiniz1@outlook.com>
2024-07-20 10:36:22 -0400

    created user entities

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt ?: Date
+}
+
+
+export class User {
+  constructor(public readonly props: UserProps){
+    this.props.createdAt =  this.props.createdAt ?? new Date()
+  }
+}
