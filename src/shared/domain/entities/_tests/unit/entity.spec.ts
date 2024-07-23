ad81a540fbbee170ffad4f70592586314f284421
elieldiniz <elieldiniz1@outlook.com>
2024-07-23 16:43:43 -0400

    abstract class entity and test

---
+++ src/shared/domain/entities/_tests/unit/entity.spec.ts
@@ -0,0 +1,47 @@
+import {validate as uuidValidate } from 'uuid'
+import { Entity } from '../../entity'
+
+
+type StubProps = {
+  prop1: string,
+  prop2: number
+}
+class StubEntity extends Entity<StubProps>{
+
+}
+describe('UserEntity init test', () => {
+
+  it('shoud set props and id', ()=>{
+    const props = {prop1: 'value1', prop2: 15}
+    const entity = new StubEntity(props)
+    // Verificar se props estão configurados corretamente
+    expect(entity.props).toStrictEqual(props)
+    // Verificar se ID não é nulo
+    expect(entity._id).not.toBeNull()
+    // Validar ID como UUID
+    expect(uuidValidate(entity._id)).toBeTruthy()
+  })
+
+  it('shoud accept a valid uuid', ()=>{
+    const props = {prop1: 'value1', prop2: 15}
+    const id = '9b67058e-9389-4061-a90c-1f3b2704c90b'
+    const entity = new StubEntity(props, id)
+    // Validar ID como UUID
+    expect(uuidValidate(entity._id)).toBeTruthy()
+    // Verificar se ID corresponde ao UUID fornecido
+    expect(entity._id).toBe(id)
+  })
+
+  it('shoud accept a valid to a javascript Object', ()=>{
+    const props = {prop1: 'value1', prop2: 15}
+    const id = '9b67058e-9389-4061-a90c-1f3b2704c90b'
+    const entity = new StubEntity(props, id)
+
+    expect(entity.toJSON()).toStrictEqual({
+      id,
+      ...props,
+    })
+  })
+
+
+});
--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -1,4 +1,4 @@
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 export abstract class Entity<Props = any> {
   public readonly _id: string
