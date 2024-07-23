12291e07f22724e011bd35c7bb827916bae6629a
elieldiniz <elieldiniz1@outlook.com>
2024-07-23 19:12:47 -0400

    Refactoring App Module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,12 +1,13 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { ConfigModule, ConfigModuleOptions} from '@nestjs/config';
 import { join } from 'node:path';
 
 
 @Module({
+  imports: [ConfigModule],
   providers: [EnvConfigService],
-
+  exports: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
