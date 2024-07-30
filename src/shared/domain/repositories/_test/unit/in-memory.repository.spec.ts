495e0a37077b70a531164bea73b362bebf9d1853
elieldiniz <elieldiniz1@outlook.com>
2024-07-30 16:51:15 -0400

    InMemoryRepository unit tests - FindAll Method

--- src/shared/domain/repositories/_test/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/_test/unit/in-memory.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Entity } from "@/shared/domain/entities/entity"
 import { InMemoryRepository } from "../../in-memory.repository"
 import { SuiteContext } from "node:test"
+import { NotFoudError } from "@/shared/domain/erros/not-foud-erros"
+import { rejects } from "node:assert"
 
 type StubeEntityProps = {
   name: string
@@ -24,4 +26,23 @@ describe('InMemoryRepositry unit tests', ()=>{
 
     expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
   })
+
+  it("should throw error when entity not", async () => {
+    await expect(sut.findByID('fakerId')).rejects.toThrow(new NotFoudError('Entity not found'));
+  })
+
+  it("shold find a entity by id",async ()=>{
+    const entity = new StubeEntity({name:'test name', price: 50 })
+    await sut.insert(entity)
+    const result = await sut.findByID(entity._id)
+    expect(entity.toJSON()).toStrictEqual(result.toJSON())
+  })
+
+  it("shold return all entities",async ()=>{
+    const entity = new StubeEntity({name:'test name', price: 50 })
+    await sut.insert(entity)
+    const result = await sut.findAll()
+    expect([entity]).toStrictEqual(result)
+  })
+
 })
