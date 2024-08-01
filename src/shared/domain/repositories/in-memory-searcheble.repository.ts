f8f88f4d4c2cd3e7ae1787049fb091c746902940
elieldiniz <elieldiniz1@outlook.com>
2024-08-01 17:53:57 -0400

    adding search method InMemorySEARCHABLErepository claa

--- src/shared/domain/repositories/in-memory-searcheble.repository.ts
+++ src/shared/domain/repositories/in-memory-searcheble.repository.ts
@@ -3,15 +3,33 @@ import { Entity } from "../entities/entity";
 import { NotFoudError } from "../erros/not-foud-erros";
 import { InMemoryRepository } from "./in-memory.repository";
 import { RepositoryInterface } from "./repository-contracts";
-import { SerchablsRepositoryInterface } from "./searchble-repository-contracts";
+import { SearchParams, SerchablsRepositoryInterface, SerchResult } from "./searchble-repository-contracts";
 
 export abstract class InMemorySearchebleRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SerchablsRepositoryInterface<E, any, any>{
 
 
-    seaech(props: any): Promise<any> {
+    seaech(props: SearchParams): Promise<SerchResult<E>> {
       throw new Error('erro')
     }
+
+    protected abstract applyFilter(
+      item: E[],
+      filter: string | null
+    ): Promise<E[]>
+
+    protected async applySort(
+      items: E[],
+      sort: string | null,
+      sortDir: string | null,
+    ): Promise<E[]>{}
+
+    protected async applyPaginate(
+      items: E[],
+      sort: SearchParams['page'],
+      sortDir: SearchParams['perPage']
+
+    ): Promise<E[]>{}
 
 }
