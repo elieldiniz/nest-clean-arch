0aa233deed04ce14b84f09c4f9c90e4b7aca12c6
elieldiniz <elieldiniz1@outlook.com>
2024-08-20 23:07:11 -0400

    adding usercase signupUserCases

---
+++ src/users/applicatio/erros/bad-requet-erros.ts
@@ -0,0 +1,7 @@
+
+export class BadRequestError extends Error{
+  constructor(public message: string){
+    super(message)
+    this.name = 'BadRequestError'
+  }
+}
--- src/users/applicatio/usecase/signup.usercase.ts
+++ src/users/applicatio/usecase/signup.usercase.ts
@@ -1,3 +1,7 @@
+import { BadRequestError } from "../erros/bad-requet-erros"
+import { UserRepository } from "../../domain/repositorys/user.repository"
+import { UserEntity } from "@/users/domain/entities/user.entity"
+
 export namespace SignupUserCase{
   export type Input = {
     name: string
@@ -5,14 +9,34 @@ export namespace SignupUserCase{
     password: string
   }
   export type Output = {
+    id: string
     name: string
     email: string
     password: string
-    created: Date
+    createdAt: Date
   }
 
-  export class SignupUserCase {
-    async execute(input: Input): Promise<Output> {}
+  export class UserCase {
+    //Injeção de userRepository
+
+    constructor(private userRepository: UserRepository.Repository ){
+
+    }
+    async execute(input: Input): Promise<Output> {
+      const {email,name, password} = input
+
+      if(!email || !name || !password) {
+        throw new BadRequestError('Input data not provided')
+    }
+
+    await this.userRepository.emailExists(email)
+
+    const entity = new UserEntity(input)
+
+    await this.userRepository.insert(entity)
+
+    return entity.toJSON()
   }
+}
 }
 
