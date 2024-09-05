59b43529af5694dcf4e6f3ff78098017b81ea6ee
elieldiniz <elieldiniz1@outlook.com>
2024-09-04 22:47:45 -0400

    Registering SignapUserCase

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { SigninUserCase } from '../applicatio/usecases/signin.usercase';
+import { UserInMemoryRepository } from './database/in-memory/repository/user-in-memory.repository';
+import { BcriptjsHashProvider } from './providers/hash-provider/bcryptjs-hash.providers';
+import { UserRepository } from '../domain/repositorys/user.repository';
+import { HashProviders } from '@/shared/application/providers/hash-provider';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService,
+  {
+    provide: 'UserRepository',
+    useClass: UserInMemoryRepository
+  },
+  {
+    provide: 'HashProviders',
+    useClass: BcriptjsHashProvider
+  },
+  {
+    provide: SigninUserCase.UserCase,
+    useFactory: (
+      userRepository: UserRepository.Repository,
+      hashProvider: HashProviders
+    )=>{
+      return new SigninUserCase.UserCase(userRepository, hashProvider)
+    },
+    inject: ['UserRepository','HashProviders']
+  }]
+
 })
 export class UsersModule {}
